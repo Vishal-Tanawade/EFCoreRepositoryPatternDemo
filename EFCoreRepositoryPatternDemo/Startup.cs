78f0f0480fd034513eead60d8a11e0792d83d61a
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-21 14:03:44 +0530

    Modified Index.cshtml of ExperiencePortal / and changed startup file

--- EFCoreRepositoryPatternDemo/Startup.cs
+++ EFCoreRepositoryPatternDemo/Startup.cs
@@ -51,7 +51,7 @@ namespace EFCoreRepositoryPatternDemo
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=ExperiencePortal}/{action=Index}/{id?}");
             });
         }
     }
