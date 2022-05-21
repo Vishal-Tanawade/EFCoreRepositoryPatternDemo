5ebe7a078bf90a43bf70c9726ee98c057fe29a93
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-21 17:59:51 +0530

    AppDBContext modified

--- EFCoreRepositoryPatternDemo/Models/AppDBContext.cs
+++ EFCoreRepositoryPatternDemo/Models/AppDBContext.cs
@@ -4,6 +4,17 @@ namespace EFCoreRepositoryPatternDemo.Models
 {
     public class AppDBContext: DbContext
     {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Seed();
+        }
+
+        public DbSet<ClsEmployee> coreEmployees { get; set; }
+        public DbSet<ClsSkill> coreSkill { get; set; }
     }
 }
